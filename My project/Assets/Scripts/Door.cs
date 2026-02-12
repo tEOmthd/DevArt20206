e53e36e8e8e2e68e2effac0c317e260a60e2ed8f
Téo <mathiaud.teo@gmail.com>
2026-02-12 16:55:06 +0100

    ma scene backup

--- My project/Assets/Scripts/Door.cs
+++ My project/Assets/Scripts/Door.cs
@@ -5,34 +5,43 @@ public class Door : MonoBehaviour
 {
     public float openAngle = 90f;
     public float openSpeed = 2f;
-    public bool isOpen = false;
 
-    private Quaternion _closedRotation;
-    private Quaternion _openRotation;
-    private Coroutine _currentCoroutine; 
+    private bool isOpen = false;
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    private Coroutine currentCoroutine;
+
     void Start()
     {
-        _closedRotation = transform.rotation; 
-        _openRotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, openAngle, 0));
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(
+            transform.eulerAngles + new Vector3(0, openAngle, 0)
+        );
     }
 
+    public void OpenDoor()
+    {
+        Debug.Log("Door activated");
 
-    // Méthode publique pour déclencher la porte depuis un autre script
-   public void OpenDoor()
-{
-    if (_currentCoroutine != null)
-        StopCoroutine(_currentCoroutine);
-    Debug.Log("Door activated");
-    _currentCoroutine = StartCoroutine(ToggleDoor());
-}
+        if (currentCoroutine != null)
+            StopCoroutine(currentCoroutine);
+
+        currentCoroutine = StartCoroutine(ToggleDoor());
+    }
+
     private IEnumerator ToggleDoor()
     {
-        Quaternion targetRotation = isOpen ? _closedRotation : _openRotation;
+        Quaternion targetRotation = isOpen ? closedRotation : openRotation;
         isOpen = !isOpen;
 
-        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.01f)
+        while (Quaternion.Angle(transform.rotation, targetRotation) > 0.1f)
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * openSpeed);
+            transform.rotation = Quaternion.Lerp(
+                transform.rotation,
+                targetRotation,
+                Time.deltaTime * openSpeed
+            );
+
             yield return null;
         }
 
