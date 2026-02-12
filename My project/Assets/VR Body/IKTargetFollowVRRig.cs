09fb549a4c9714340cbdcd990243955737c34e7d
Samuel <samuellefevre34@gmail.com>
2026-02-12 19:11:40 +0100

    fix

--- My project/Assets/VR Body/IKTargetFollowVRRig.cs
+++ My project/Assets/VR Body/IKTargetFollowVRRig.cs
@@ -26,17 +26,14 @@ public class IKTargetFollowVRRig : MonoBehaviour
     public float headBodyYawOffset;
 
     // Update is called once per frame
-   void LateUpdate()
-{
-    head.Map();
-    
-    transform.position = head.ikTarget.position + headBodyPositionOffset;
-    float yaw = head.vrTarget.eulerAngles.y;
-    transform.rotation = Quaternion.Lerp(transform.rotation,
-        Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),
-        turnSmoothness);
+    void LateUpdate()
+    {
+        transform.position = head.ikTarget.position + headBodyPositionOffset;
+        float yaw = head.vrTarget.eulerAngles.y;
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z),turnSmoothness);
 
-    leftHand.Map();
-    rightHand.Map();
-}
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
 }
